ef9acdbd5b5de0d46201236da0009b1576471f47
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-10 15:10:12 +0300

    Imported appLoading

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -24,97 +25,16 @@ export default function App() {
       <AppLoading
         startAsync={fetchFonts}
         onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
+        onError={err => console.log(err)}
       />
     );
   }
-
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
     setUserNumber(null);
   };
-  import React, { useState } from 'react';
-  import { StyleSheet, View } from 'react-native';
-  import * as Font from 'expo-font';
-  import { AppLoading } from 'expo';
-  
-  import Header from './components/Header';
-  import StartGameScreen from './screens/StartGameScreen';
-  import GameScreen from './screens/GameScreen';
-  import GameOverScreen from './screens/GameOverScreen';
-  
-  const fetchFonts = () => {
-    return Font.loadAsync({
-      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-    });
-  };
-  
-  export default function App() {
-    const [userNumber, setUserNumber] = useState();
-    const [guessRounds, setGuessRounds] = useState(0);
-    const [dataLoaded, setDataLoaded] = useState(false);
-  
-    if (!dataLoaded) {
-      return (
-        <AppLoading
-          startAsync={fetchFonts}
-          onFinish={() => setDataLoaded(true)}
-          onError={err => console.log(err)}
-        />
-      );
-    }
-  
-    const configureNewGameHandler = () => {
-      setGuessRounds(0);
-      setUserNumber(null);
-    };
-  
-    const startGameHandler = selectedNumber => {
-      setUserNumber(selectedNumber);
-    };
-  
-    const gameOverHandler = numOfRounds => {
-      setGuessRounds(numOfRounds);
-    };
-  
-    let content = <StartGameScreen onStartGame={startGameHandler} />;
-    content = (
-      <GameOverScreen
-        roundsNumber={1}
-        userNumber={1}
-        onRestart={configureNewGameHandler}
-      />
-    );
-    if (userNumber && guessRounds <= 0) {
-      content = (
-        <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-      );
-    } else if (guessRounds > 0) {
-      content = (
-        <GameOverScreen
-          roundsNumber={guessRounds}
-          userNumber={userNumber}
-          onRestart={configureNewGameHandler}
-        />
-      );
-    }
-  
-    return (
-      <View style={styles.screen}>
-        <Header title="Guess a Number" />
-        {content}
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    screen: {
-      flex: 1
-    }
-  });
-  
+
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
   };
@@ -124,7 +44,13 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
-
+  content = (
+    <GameOverScreen
+      roundsNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
