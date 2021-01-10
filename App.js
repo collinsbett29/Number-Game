1f92691934d088d9abb62fd3b51e162e0e555015
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-10 10:21:24 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import GameOverScreen from './screens/GameOverScreen';
+import Header from './components/Header';
+import StartGameScreen from './screens/startGameScreen';
+import GameScreen from './screens/GameScreen';
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>;
+  }
+  else if (guessRounds > 0){
+    content = <GameOverScreen />;
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      {content}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
